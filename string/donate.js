53c015d1ef2a3f9a247df4ba099b8b2f6c9d00f8
Lalairul704 <110320408+Lalairul704@users.noreply.github.com>
2023-04-30 06:12:19 +0800

    Update donate.js

--- string/donate.js
+++ string/donate.js
@@ -1,10 +1,8 @@
 const chalk = require('chalk')
 const fs = require('fs')
 global.donate = `
-GOPAY :
-DANA :
-OVO :
-PULSA :`
+DANA : 085-904-284-057
+PULSA AXIS : 083-838-057-050`
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
