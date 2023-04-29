e760cca94f399f18c5a07bd74e122f3a9f4bffde
Lalairul704 <110320408+Lalairul704@users.noreply.github.com>
2023-04-30 06:30:10 +0800

    Update tunggu.js

--- string/tunggu.js
+++ string/tunggu.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk')
 const fs = require('fs')
-global.tunggu = `Silahkan Tunggu Permintaan Mu sedang kami proses Okei,`
+global.tunggu = `Silahkan Tunggu Permintaan Mu sedang kami Proses !,`
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
